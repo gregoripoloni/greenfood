f138d9211ba87f89653c2d640c5c60d17b51248f
Grégori Poloni Machado <104400655+gregoripoloni@users.noreply.github.com>
2025-05-09 20:07:04 -0300

    Inicia tela home

--- src/main/controller/HomeController.java
+++ src/main/controller/HomeController.java
@@ -1,17 +1,45 @@
 package main.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
+import javafx.stage.Stage;
 import main.MainApp;
+import usuario.Doador;
 import usuario.Usuario;
+
+import java.io.IOException;
 
 public class HomeController {
     @FXML
-    private Label welcomeLabel;
+    private Button logoutMenu;
+    @FXML
+    private Button foodButton;
+    @FXML
+    private Button donationButton;
 
     public void initialize() {
         Usuario usuario = MainApp.getUser();
-        welcomeLabel.setText("Bem vindo, " + usuario.getNome());
+
+        if (usuario.getTipo().equals(Doador.TIPO)) {
+            foodButton.setText("Cadastrar alimento");
+            donationButton.setText("Cadastrar doação");
+        } else {
+            foodButton.setVisible(false);
+            donationButton.setText("Consultar doações");
+        }
+    }
+
+    public void logOut(ActionEvent actionEvent) throws IOException {
+        MainApp.setUser(null);
+
+        Stage stage = (Stage) logoutMenu.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/main/view/login.fxml"));
+        stage.setTitle("Greenfood - LogIn");
+        stage.setScene(new Scene(root));
     }
 }
