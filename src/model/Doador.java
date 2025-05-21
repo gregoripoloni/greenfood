01497bb2c01148f5681b473f20384d0787581f26
larimasca <larissacmascarello@gmail.com>
2025-05-21 19:34:42 -0300

    doacaoes quase 100%
    arrumei o que precisava agora a home tambem funciona pra cadastro de doacao, só tem que colocar um botao tambem de relatorio ai
    
    fiz att tambem em uma partezinha do doador pra mim usar mas comentei no codigo 👍

--- src/model/Doador.java
+++ src/model/Doador.java
@@ -1,5 +1,10 @@
 package model;
 
+import alimentos.Alimento;
+import alimentos.Estoque;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Doador extends Usuario {
     public static final String TIPO = "doador";
     private Estoque estoque;
@@ -19,6 +24,16 @@ public class Doador extends Usuario {
 	public void setEstoque(Estoque estoque) {
 		this.estoque = estoque;
 	}
+
+    //att lari, coloquei esse aqui em baixo para controlar la na parte de doacao :)
     
-    
+    private List<Alimento> alimentos = new ArrayList<>();
+
+	public List<Alimento> getAlimentos() {
+	    return alimentos;
+	}
+
+	public void adicionarAlimento(Alimento alimento) {
+	    alimentos.add(alimento);
+	}
 }
